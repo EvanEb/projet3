9952e28e519c7327aec1cda400260dbbb58dd712
LordKuss <tchoupe.tchoupi@gmail.com>
2022-01-21 08:35:50 +0100

    ;.

--- planning.php
+++ planning.php
@@ -1,5 +1,5 @@
 <?php
-// Initialiser la session
+	// Initialiser la session
 	session_start();
 	// Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
 	if(!isset($_SESSION["username"])){
@@ -8,7 +8,55 @@
 	}
 ?>
 <!DOCTYPE html>
-    <html>
-        <p> Voici le planning </p>
+<html>
+<head>
+<meta charset='utf-8' />
+<link href='../lib/main.css' rel='stylesheet' />
+<script src='../lib/main.js'></script>
+<script>
 
-    </html>
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialDate: '2020-09-12',
+      initialView: 'timeGridWeek',
+      nowIndicator: true,
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      },
+      navLinks: true, 
+      editable: true,
+      selectable: true,
+      selectMirror: true,
+      dayMaxEvents: true,
+    });
+
+    calendar.render();
+  });
+
+</script>
+<style>
+
+  body {
+    margin: 40px 10px;
+    padding: 0;
+    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+    font-size: 14px;
+  }
+
+  #calendar {
+    max-width: 1100px;
+    margin: 0 auto;
+  }
+
+</style>
+</head>
+<body>
+
+  <div id='calendar'></div>
+
+</body>
+</html>
